2c66b7a099500750fca45a594a7034645399b6a1
Stephanie <stephanie.enciso13@gmail.com>
2021-03-16 18:24:24 -0500

    Completed the seed, and the update and delete endpoints for Stretch.

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -3,7 +3,9 @@ const db = require('../../data/config-db')
 module.exports = {
     find,
     findById,
-    add
+    add,
+    update,
+    remove
 }
 
 function find() {
@@ -17,4 +19,14 @@ function findById(id) {
 async function add(car) {
    const [id] = await db('cars').insert(car)
    return findById(id)
+}
+
+async function update(id, car) {
+    await db('cars').where('car_id', id).update(car)
+    const newCar = await db('cars').where('car_id', id).first()
+    return newCar
+}
+
+function remove(id) {
+    return db('cars').where('car_id', id).del()
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -54,4 +54,50 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.put('/:id', async (req, res) => {
+    const { VIN, Make, Model, Mileage } = req.body
+    if(!VIN || !Make || !Model || !Mileage) {
+        res.status(400).json({
+            message: 'VIN, Make, Model, and Mileage are required fields.'
+        })
+    } else {
+        try{
+            const validId = await Car.findById(req.params.id)
+            if(!validId) {
+                res.status(404).json({
+                    message: 'The car with the given id does not exist.'
+                })
+            } else {
+                const car = await Car.update(req.params.id, req.body)
+                res.status(200).json(car)
+            }
+        } catch (err) {
+            res.status(500).json({
+                message: 'The car could not be updated',
+                actualError: err
+            })
+        }
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    try{
+        const removedCar = await Car.findById(id)
+        if(!removedCar) {
+            res.status(404).json({
+                message: 'The car with a given ID does not exist.'
+            })
+        } else {
+            const deleteCar = await Car.remove(id)
+            res.status(200).json(removedCar)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'The car could not be deleted',
+            actualError: err.message
+        })
+    }
+})
+
 module.exports = router
