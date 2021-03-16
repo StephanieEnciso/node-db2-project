3f22b7957c7242f995148a079613ee2016ef4896
Stephanie <stephanie.enciso13@gmail.com>
2021-03-16 15:57:57 -0500

    Initial commit, project and it's files are set up.

---
+++ api/cars/cars-model.js
@@ -0,0 +1,29 @@
+const db = require('../../data/config-db')
+
+module.exports = {
+    find,
+    findById,
+    add,
+    update,
+    remove
+}
+
+function find() {
+
+}
+
+function findById(id) {
+    
+}
+
+function add(car) {
+    
+}
+
+function update(id, car) {
+    
+}
+
+function remove(id) {
+    
+}
