2a173853cf4ff38220dfda933b7889747b7c3972
Stephanie <stephanie.enciso13@gmail.com>
2021-03-16 17:01:29 -0500

    Completed MVP, both get endpoints and the post endpoint are working

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -17,4 +17,41 @@ router.get('/', (req, res) => {
       })
 })
 
+router.get('/:id', async (req, res) => {
+    try{
+        const car = await Car.findById(req.params.id)
+        if(!car) {
+            res.status(404).json({
+                message: 'The car with the given id does not exist.'
+            })
+        } else {
+            res.status(200).json(car)
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: 'The car could not be retrieved.',
+            actualError: err
+        })
+    }
+})
+
+router.post('/', async (req, res) => {
+    const { VIN, Make, Model, Mileage } = req.body
+    if(!VIN || !Make || !Model || !Mileage) {
+        res.status(400).json({
+            message: 'VIN, Make, Model, and Mileage are required fields.'
+        })
+    } else {
+        try{
+            const newCar = await Car.add(req.body)
+            res.status(201).json(newCar)
+        } catch (err) {
+            res.status(500).json({
+                message: 'The car could not be added to the database',
+                actualError: err
+            })
+        }
+    }
+})
+
 module.exports = router
